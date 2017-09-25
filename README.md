c493f9aaf2a12f659ef898a27b71083bc2c1da9a
Ildar Sagdejev <specious@gmail.com>
2017-09-25 11:48:49 -0400

    Added header with your name on it

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Generate an HTML page showing all of your Facebook friends and click them to visit their profiles.
+Generate an HTML page of all of your Facebook friends so you can click them to visit their profiles.
 
 ![Screenshot](screenshot.png)
 
@@ -9,14 +9,17 @@ First, `yarn install` dependencies. Then:
 - Go to: https://www.facebook.com/me/friends
 - Scroll down, repeatedly, until all of your friends are loaded
 - Save the page as `in.html`
-- *(Optional)* For the ultimate experience, run `in.html` through [inliner](https://github.com/remy/inliner) to create a self-contained HTML file
+- *(Optional)* For the ultimate experience, use [inliner](https://github.com/remy/inliner) to create a self-contained HTML file:
+  ```
+  inliner in.html > out.html && mv out.html in.html
+  ```
 - Run `./index.js in.html > friends.html`
 
-Open `friends.html` in your browser. Enjoy Facebook!
+Open `friends.html` in your browser.
 
 ## Does this work with my friend's friends?
 
-Yes it does.
+Yes it does. Try it with: https://www.facebook.com/tknomad/friends
 
 ## License
 
--- index.js
+++ index.js
@@ -33,7 +33,27 @@ function readSource(src) {
 }
 
 readSource(src)
-  .find('li._698') // friend element
+  .find('#fbProfileCover')
+  .set({
+    'avatar': 'img.profilePic@src',
+    'name': '#fb-timeline-cover-name',
+    'link': 'a._2nlw@href'
+  })
+  .data(function(data) {
+    console.log('<div id="header">');
+    console.log('  <a href="' + data.link + '"><img src="' + data.avatar + '" width="42"></a>');
+    console.log('  <div style="display:inline-block">');
+    console.log('    <h3 style="margin:8px">' + data.name + "'s facebook friends" + '</h3>');
+    console.log('    <h4 style="margin:8px">Generated with: <a href="https://github.com/specious/facebook-friends">github.com/specious/facebook-friends</a></h4>');
+    console.log('  </div>');
+    console.log('</div>');
+  })
+  // Reset the data object
+  //   ( ...as suggested by @rchipka in: https://github.com/rchipka/node-osmosis/issues/21#issuecomment-93947759 )
+  .then(function(context, data, next) {
+    next(context, {});
+  })
+  .find('li._698') // each friend element
   .set({
     'avatar': 'img._s0@src',
     'name': 'img._s0@aria-label',
